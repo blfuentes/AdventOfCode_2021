3586e5052e5bcbaece6ce1ec2d7e659626d74803
blfuentes <epiphonetodie@gmail.com>
2021-12-03 21:25:49 +0100

    Playground file added

---
+++ Playground.fsx
@@ -0,0 +1,16 @@
+﻿List.iter (printf "%d") [0 .. 9]
+List.iter 
+    (printf "%d") 
+        [0 .. 9]
+
+let calculateRate(input: list<string[]>, criteria: string, noncriteria: string, op: (int -> int -> bool)) =
+    let rate = [0 .. input.Head.Length - 1] 
+            |> List.map(fun idx -> input 
+                                |> List.filter (fun e -> e.[idx] = criteria) 
+                                |> List.length)
+
+    rate |> List.map(fun r -> if op r (input.Length - r) then criteria else noncriteria) |> List.toArray
+
+let fakeinput = [[|"0";"0";"0";"1"|];[|"0";"0";"1";"0"|];[|"0";"0";"1";"1"|];[|"0";"1";"0";"0"|];]
+let rate = calculateRate(fakeinput, "1", "0", (>=))
+rate
